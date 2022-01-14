395c0fb0767297ed8a101b64f2a9c11f819858ea
Omar <omaralzghoul00@gmail.com>
2022-01-14 16:26:49 +0200

    commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:brew_crew/screens/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/models/user.dart';
+
+// void main() => runApp(MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User1?>.value(
+      value: AuthService().user,
+      initialData: null,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
+}
